02e903c9906618720189d19415e1054e92ff6343
hepta.luigi.o <hepta.luigi.o@NRJPRF13294464>
2023-06-23 19:15:57 -0300

    Atualização nos métodos GET, PUT, POST.

--- EmpresasFornecedoresCRUD/Controllers/EmpresaController.cs
+++ EmpresasFornecedoresCRUD/Controllers/EmpresaController.cs
@@ -22,12 +22,39 @@ namespace EmpresasFornecedoresCRUD.Controllers
             _configuration = configuration;
         }
 
+
+
+        [HttpPost]
+        public IActionResult Post(Empresa emp)
+        {
+            string query = @"
+        insert into Empresa(CNPJ, Nome_Fantasia, Cep, Estado) values
+        (@Cnpj, @Nome_Fantasia, @Cep, @Estado)
+    ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Default");
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@Cnpj", emp.Cnpj);
+                myCommand.Parameters.AddWithValue("@Nome_Fantasia", emp.Nome_Fantasia);
+                myCommand.Parameters.AddWithValue("@Cep", emp.Cep);
+                myCommand.Parameters.AddWithValue("@Estado", emp.Estado);
+                myCon.Open();
+                myCommand.ExecuteNonQuery();
+            }
+
+            return Ok();
+        }
+
         [HttpGet]
         public JsonResult Get()
         {
             string query = @"
-    select Id, CNPJ, Nome_Fantasia, Cep, Estado from empresa
-    ";
+            select Id, CNPJ, Nome_Fantasia, Cep, Estado from empresa
+            ";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("Default");
 
@@ -59,5 +86,30 @@ namespace EmpresasFornecedoresCRUD.Controllers
             return new JsonResult(rows);
         }
 
+        [HttpPut]
+        public IActionResult Put(Empresa emp)
+        {
+            string query = @"
+    update Empresa set CNPJ = @Cnpj, Nome_Fantasia = @Nome_Fantasia, Cep = @Cep, Estado = @Estado where Id = @Id
+    ";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Default");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@Cnpj", emp.Cnpj);
+                myCommand.Parameters.AddWithValue("@Nome_Fantasia", emp.Nome_Fantasia);
+                myCommand.Parameters.AddWithValue("@Cep", emp.Cep);
+                myCommand.Parameters.AddWithValue("@Estado", emp.Estado);
+                myCommand.Parameters.AddWithValue("@Id", emp.Id);
+                myCon.Open();
+                myCommand.ExecuteNonQuery();
+            }
+
+            return Ok();
+        }
+
     }
 }
+
